b9a431259eb38cfa473a4df1762da3280dd2f1e5
k <mol.tired@gmail.com>
2021-05-07 18:25:42 -0500

    menu: fix: <a> cannot appear as a descendant of <a>

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,46 +1,41 @@
 import { FunctionComponent } from 'react';
-import {
-  NavLink,
-  HashRouter,
-} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import AboutSVG from 'components/svg/AboutSVG';
 import HomeSVG from 'components/svg/HomeSVG';
 import style from './Menu.module.scss';
 
 const Menu: FunctionComponent = () => {
   return (
-    <HashRouter>
-      <ol className={style.menu}>
-        <li className={`${style.menu__item} ${style.menu__itemSelected}`}>
-          <NavLink to='/'>
-            <Home />
-          </NavLink>
-        </li>
-        <li className={style.menu__item}>
-          <NavLink to='/about'>
-            <About />
-          </NavLink>
-        </li>
-      </ol>
-    </HashRouter>
+    <ol className={style.menu}>
+      <li className={`${style.menu__item} ${style.menu__itemSelected}`}>
+        <NavLink exact to='/'>
+          <Home />
+        </NavLink>
+      </li>
+      <li className={style.menu__item}>
+        <NavLink to='/about'>
+          <About />
+        </NavLink>
+      </li>
+    </ol>
   );
 };
 
 const Home: FunctionComponent = () => {
   return (
-    <a className={style.menu__link} href="/">
+    <div className={style.menu__link}>
       <HomeSVG className={style.menu__icon} />
       <span className={style.menu__label}>Home</span>
-    </a>
+    </div>
   );
 };
 
 const About: FunctionComponent = () => {
   return (
-    <a className={style.menu__link} href="/about">
+    <div className={style.menu__link}>
       <AboutSVG className={style.menu__icon} />
       <span className={style.menu__label}>About</span>
-    </a>
+    </div>
   );
 };
 
