048f8abe01471671f0502e3c6e97ee33968d63ee
k <mol.tired@gmail.com>
2021-05-07 18:30:54 -0500

    app: render the Main scene

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,26 +1,11 @@
 import { FunctionComponent } from 'react';
-import Sypnosis from 'components/Sypnosis';
-import Details from 'components/Details';
-import { SYPNOSIS, SypnosisProps } from '../../mock-sypnosis';
+import Main from 'scenes/Main';
 
 const App: FunctionComponent = () => {
-  const sypnosis: Array<SypnosisProps> = SYPNOSIS; 
-  const sypnosisComponents: Array<JSX.Element> = [];
-
-  for (const s of sypnosis) {
-    sypnosisComponents.push(
-      <Sypnosis 
-        imgUrl={s.imgURL} title={s.title} 
-        subtitle={s.subtitle} time={s.time} categories={s.categories}
-      />
-    );
-  }
-
   return (
-    <div>
-      <Details />
-      {sypnosisComponents}
-    </div>
+    <>
+      <Main />
+    </>
   );
 };
 
