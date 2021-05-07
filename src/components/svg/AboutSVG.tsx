08347fb7a6bdfac5670667d0067d28582d89104a
k <mol.tired@gmail.com>
2021-05-07 18:25:02 -0500

    svg: fix class names capitalization

--- src/components/svg/AboutSVG.tsx
+++ src/components/svg/AboutSVG.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react';
 const AboutSVG: FunctionComponent<{ className: string }> = ({ className }) => {
   return (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
-      stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
       <path stroke="none" d="M0 0h24v24H0z"></path>
       <circle cx="12" cy="7" r="4"></circle>
       <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
--- src/components/svg/ClockSVG.tsx
+++ src/components/svg/ClockSVG.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react';
 const ClockSVG: FunctionComponent<{ className: string }> = ({ className }) => {
   return (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
-      stroke-width="2" stroke="currentColor" fill="none" /* color */ stroke-linecap="round" stroke-linejoin="round">
+      strokeWidth="2" stroke="currentColor" fill="none" /* color */ strokeLinecap="round" strokeLinejoin="round">
       <path stroke="none" d="M0 0h24v24H0z"></path>
       <circle cx="12" cy="12" r="9"></circle>
       <polyline points="12 7 12 12 15 15"></polyline>
