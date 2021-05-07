db4bba9c29dbbde5ea495a5498d796c127d91396
k <mol.tired@gmail.com>
2021-05-07 18:31:23 -0500

    main: add the Main component scene

---
+++ src/scenes/Main/index.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from 'react';
+import { Route, HashRouter } from 'react-router-dom';
+import Details from 'components/Details';
+import Menu from 'components/Menu';
+import About from 'pages/About';
+import Home from 'pages/Home';
+import style from './Main.module.scss';
+
+const Main: FunctionComponent = () => {
+  return (
+    <main className={style.mainScene}>
+      <HashRouter>
+        <aside className={style.mainScene__aside}>
+          <Details />
+          <Menu />
+        </aside>
+        <div className={style.mainScene__main}>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </div>
+      </HashRouter>
+    </main>
+  );
+};
+
+export default Main;
