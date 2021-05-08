09d715b0483dd3fd6a7eb91f4a0e628d3b1227e8
k <mol.tired@gmail.com>
2021-05-08 12:37:07 -0500

    about: add no dynamic content

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,67 @@
 import { FunctionComponent } from 'react';
+import style from './About.module.scss';
+import ClockSVG from 'components/svg/ClockSVG';
 
 const About: FunctionComponent = () => {
   return (
-    <div>
-      About Page
+    <div className={style.about}>
+      <div className={style.about__resume}>
+        <Resume 
+          title={'About'} 
+          subtitle={"Hugo, the world's fastest framework for building websites"} 
+        />
+        <Footer time={'Feb 28, 2019'} />
+        <p className={style.about__copy}>
+          Written in Go, Hugo is an open source static site generator available under the Apache Licence 2.0. Hugo supports TOML, YAML and JSON data file types, Markdown and HTML content files and uses shortcodes to add rich content. Other notable features are taxonomies, multilingual mode, image processing, custom output formats, HTML/CSS/JS minification and support for Sass SCSS workflows.
+        </p>
+        <p className={style.about__copy}>
+          Hugo makes use of a variety of open source projects including:
+        </p>
+        <ul className={style.about__list}>
+          <li>https://github.com/yuin/goldmark</li>
+          <li>https://github.com/alecthomas/chroma</li>
+          <li>https://github.com/muesli/smartcrop</li>
+          <li>https://github.com/spf13/cobra</li>
+          <li>https://github.com/spf13/viper</li>
+        </ul>
+        <p className={style.about__copy}>
+          Hugo is ideal for blogs, corporate websites, creative portfolios, online magazines, single page applications or even a website with thousands of pages.
+        </p>
+        <p className={style.about__copy}>
+          Hugo is for people who want to hand code their own website without worrying about setting up complicated runtimes, dependencies and databases.
+        </p>
+        <p className={style.about__copy}>
+          Websites built with Hugo are extremelly fast, secure and can be deployed anywhere including, AWS, GitHub Pages, Heroku, Netlify and any other hosting provider.
+        </p>
+      </div>
     </div>
+  );
+};
+
+const Resume: FunctionComponent<{ title: string, subtitle: string }> = ({ title, subtitle }) => {
+  return (
+    <>
+      <h2 className={style.about__title}>
+        <a className={style.about__titleLink}
+          href="/" >
+          {title}
+        </a>
+      </h2>
+      <h3 className={style.about__subtitle}>
+        {subtitle}
+      </h3>
+    </>
+  );
+};
+
+const Footer: FunctionComponent<{ time: string }> = ({ time }) => {
+  return (
+    <footer className={style.about__footer}>
+      <ClockSVG className={style.about__svg} />
+      <time className={style.about__time}>
+        {time}
+      </time>
+    </footer>
   );
 };
 
