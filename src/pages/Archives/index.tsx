bb98e653460794919ae9316f42b8a9f94562db17
k <mol.tired@gmail.com>
2021-05-13 17:17:47 -0500

    add archives page

---
+++ src/pages/Archives/index.tsx
@@ -0,0 +1,10 @@
+import CategoryList from 'components/CategoryList';
+
+export default function Archives() {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <CategoryList />
+    </div>
+  );
+}
