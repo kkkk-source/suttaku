d6303944179fcc04f10169db232f4c30748d08ed
k <mol.tired@gmail.com>
2021-05-15 15:30:00 -0500

    add archives page styles

--- src/pages/Archives/index.tsx
+++ src/pages/Archives/index.tsx
@@ -1,8 +1,9 @@
 import CategoryList from 'components/CategoryList';
+import styles from './Archives.module.scss';
 
 export default function Archives() {
   return (
-    <div>
+    <div className={styles.archives}>
       <h2>Categories</h2>
       <CategoryList />
     </div>
