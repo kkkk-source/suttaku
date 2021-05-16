469516cb32053ccaa53ee74fa28e484e334c160c
k <mol.tired@gmail.com>
2021-05-16 14:02:11 -0500

    change Posts page to BlogPosts

--- src/pages/BlogPosts/index.tsx
+++ src/pages/BlogPosts/index.tsx
@@ -1,14 +1,14 @@
-import PostSummary from 'components/PostSummary';
-import { getPostSummary, PostSummaryResponse } from 'services/PostService';
+import BlogPostShort from 'components/BlogPostShort';
+import { getBlogPostShort, BlogPostShortResponse } from 'services/PostService';
 
 export default function Posts(): JSX.Element  {
-  const postsSummary: Array<PostSummaryResponse> = getPostSummary(); 
+  const postsSummary: Array<BlogPostShortResponse> = getBlogPostShort(); 
   const postSummaryJsxElements: Array<JSX.Element> = [];
 
   for (let i = 0; i < postsSummary.length; i++) {
     const post = postsSummary[i];
     postSummaryJsxElements.push(
-      <PostSummary 
+      <BlogPostShort 
         key={i} 
         imgUrl={post.imgUrl} 
         categories={post.categories}
