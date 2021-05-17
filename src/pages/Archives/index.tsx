b8a520898e1d115bc06a3a8c1f4a26eeebc2ae49
k <mol.tired@gmail.com>
2021-05-17 11:23:22 -0500

    use CategoryScrollList

--- src/pages/Archives/index.tsx
+++ src/pages/Archives/index.tsx
@@ -1,11 +1,11 @@
-import CategoryList from 'components/CategoryList';
+import CategoryScrollList from 'components/CategoryScrollList';
 import styles from './Archives.module.scss';
 
 export default function Archives() {
   return (
     <div className={styles.archives}>
       <h2>Categories</h2>
-      <CategoryList />
+      <CategoryScrollList />
     </div>
   );
 }
