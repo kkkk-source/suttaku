e4fe84fc70dd82a23fe9bebd512534cb1309ce7c
k <mol.tired@gmail.com>
2021-05-17 11:44:33 -0500

    style BlogPost

--- src/components/BlogPost/index.tsx
+++ src/components/BlogPost/index.tsx
@@ -2,18 +2,29 @@ import { useParams } from 'react-router-dom';
 import { getBlogPostByTitle } from 'services/PostService';
 import RoundBox from 'components/RoundBox';
 import CategoryLinkList from 'components/CategoryLinkList';
+import ClockSVG from 'components/svg/ClockSVG';
+import styles from './BlogPost.module.scss';
 
 export default function BlogPost() {
   const { title } = useParams<{ title: string }>();
   const post = getBlogPostByTitle(title);
   return (
     <RoundBox>
-      <div>
-        <h2>{post!.title}</h2>
-        <h3>{post!.subtitle}</h3>
-        <CategoryLinkList
-          categories={post!.categories} 
-        />
+      <div className={styles.blogPost}>
+        <header>
+          <img src={post!.imgUrl} alt={''}/>
+        </header>
+        <div>
+          <CategoryLinkList
+            categories={post!.categories} 
+          />
+          <h2>{post!.title}</h2>
+          <h3>{post!.subtitle}</h3>
+          <footer>
+            <ClockSVG />
+            <time>{post!.time}</time>
+          </footer>
+        </div>
       </div>
     </RoundBox>
   );
