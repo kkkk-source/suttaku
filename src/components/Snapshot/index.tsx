80f13c3de1325dc40ef99b03c53cdb120b64bb65
k <mol.tired@gmail.com>
2021-05-21 15:49:47 -0500

    pass the props to the return statement

--- src/components/Snapshot/index.tsx
+++ src/components/Snapshot/index.tsx
@@ -1,14 +1,16 @@
+import { fmtDate } from 'services/FmtService';
 import styles from './Snapshot.module.scss';
 
-export default function Snapshot({ title, time, imgUrl }: { title?: string, time?: string, imgUrl?: string }) {
+export default function Snapshot({ title, date, imgUrl }: { title: string, date: string, imgUrl: string }) {
+  const formatedDate = fmtDate(date);
   return (
     <article className={styles.snapshot}>
       <a href="/#">
         <div>
-          <h4>Chinese Test</h4>
-          <time>Sep 09, 2020</time>
+          <h4>{title}</h4>
+          <time>{formatedDate}</time>
         </div>
-        <img src="https://d33wubrfki0l68.cloudfront.net/e7b81c3834dbdf86da47bd55be8f171a2faff64d/c9cf2/p/test-chinese/helena-hertz-wwzzxldpmog-unsplash_hu45a5e3ad5e058da6a00650ed8fd40bea_15530_800x0_resize_q75_box.jpg" alt=""/>
+        <img src={imgUrl} alt={title} />
       </a>
     </article>
   );
