2d9c1d2d28c00e01d122f1450e4ed0336e87890a
k <mol.tired@gmail.com>
2021-05-23 14:50:27 -0500

    render BlogPosts sort by category

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,10 +1,29 @@
+import { getBlogPostsOfCategory } from 'services/PostService';
 import { useParams } from 'react-router-dom';
 
 export default function Category() {
   const { name } = useParams<{ name: string }>();
+  const blogPosts = getBlogPostsOfCategory(name);
+  const blogPostsJSX: Array<JSX.Element> = [];
+
+  for (let i = 0; i < blogPosts.length; i++) {
+    blogPostsJSX.push(<BlogPost key={i} title={blogPosts[i].title} />);
+  }
+
   return (
-    <h3>
-      {name}
-    </h3>
+    <div>
+      <h3>
+        {name}
+      </h3>
+      {blogPostsJSX}
+    </div>
+  );
+}
+
+function BlogPost({ title }: { title: string }) {
+  return (
+    <div>
+      {title}
+    </div>
   );
 }
