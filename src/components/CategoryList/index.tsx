0893e077ef3dfae0e5cf58037bc44dabd31ab100
k <mol.tired@gmail.com>
2021-05-23 16:59:27 -0500

    add imgUrl and cssGradient properties to CATEGORIES

--- src/components/CategoryList/index.tsx
+++ src/components/CategoryList/index.tsx
@@ -3,29 +3,30 @@ import { getCaterogies } from 'services/CategoryService';
 import styles from './CategoryList.module.scss';
 
 export default function CategoryList(): JSX.Element {
-  const categories: Array<JSX.Element> = [];
-  for (let name in getCaterogies()) {
-    categories.push(<Category key={name} name={name} />);
+  const categories = getCaterogies()
+  const categoriesJSX: Array<JSX.Element> = [];
+  for (let name in categories) {
+    categoriesJSX.push(<Category key={name} name={name} imgUrl={categories[name].imgUrl} cssGradient={categories[name].cssGradient} />);
   }
 
   return (
     <div className={styles.categoryList}>
-      {categories}
+      {categoriesJSX}
     </div>
   );
 }
 
-function Category({ name }: { name: string }): JSX.Element {
+function Category({ name, imgUrl, cssGradient }: { name: string, imgUrl: string, cssGradient: string }): JSX.Element {
   return (
     <article>
       <Link to={`c/${name}`}>
         <div>
-          <img 
-            src="https://d33wubrfki0l68.cloudfront.net/854bc7c7847a97f4fab1ed9f1fd0d603f7c64ccc/05580/categories/test/_hu8102ac0a5989befdf52fa5096a373f78_52927_ba0b8e744b36f1e48b9c54b5d2847004.jpg" 
+          <img
+            src={imgUrl}
             alt={`category ${name}`} 
           />
         </div>
-        <div>
+        <div style={{ background: cssGradient }}>
           <h3>
             {name}
           </h3>
