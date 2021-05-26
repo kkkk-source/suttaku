76ffb8068a0544d073bd6426214d57954e2663b0
k <mol.tired@gmail.com>
2021-05-26 00:46:00 -0500

    call CategoryFact

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,12 +1,13 @@
 import { useParams } from 'react-router-dom';
 import { getBlogPostsOfCategory } from 'services/PostService';
 import BlogPostList, { BlogPost } from 'components/BlogPostList';
+import CategoryFact from 'components/CategoryFact';
 
 export default function Category() {
   const { name } = useParams<{ name: string }>();
   return (
     <div>
-      <h3>{name}</h3>
+      <CategoryFact name={name} />
       <BlogPostList 
         fn={() => ((): Array<BlogPost> => getBlogPostsOfCategory(name))()} 
       />
