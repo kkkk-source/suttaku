ddff2a0d2d75c35e975248f699d3a073330309bb
k <mol.tired@gmail.com>
2021-05-26 00:45:14 -0500

    add getCategoryByName and add category description

--- src/services/CategoryService/index.ts
+++ src/services/CategoryService/index.ts
@@ -4,11 +4,11 @@ import categoryC from 'assets/images/categoryC.jpg';
 import categoryD from 'assets/images/categoryD.jpg';
 
 // key - category name; value - hexadecimal code color
-const CATEGORIES: { [name: string]: { color: string, imgUrl: string, cssGradient: string } } = {
-  syntax: { color: '#df7988', imgUrl: categoryA, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(91, 158, 204, 0.75) 100%) repeat scroll 0% 0%' },
-  试:     { color: '#2a9d8f', imgUrl: categoryB, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(0, 177, 106, 0.65) 100%) repeat scroll 0% 0%' },
-  themes: { color: '#ffb900', imgUrl: categoryC, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(246, 71, 71, 0.65) 100%) repeat scroll 0% 0%' },
-  test:   { color: '#8ea885', imgUrl: categoryD, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(91, 158, 204, 0.75) 100%) repeat scroll 0% 0%' },
+const CATEGORIES: { [name: string]: { color: string, imgUrl: string, cssGradient: string, description: string } } = {
+  syntax: { color: '#df7988', imgUrl: categoryA, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(91, 158, 204, 0.75) 100%) repeat scroll 0% 0%', description: 'This is an example description' },
+  试:     { color: '#2a9d8f', imgUrl: categoryB, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(0, 177, 106, 0.65) 100%) repeat scroll 0% 0%', description: 'This is an example description' },
+  themes: { color: '#ffb900', imgUrl: categoryC, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(246, 71, 71, 0.65) 100%) repeat scroll 0% 0%', description: 'This is an example description' },
+  test:   { color: '#8ea885', imgUrl: categoryD, cssGradient: 'rgba(0, 0, 0, 0) linear-gradient(0deg, rgba(86, 86, 102, 0.5) 0%, rgba(91, 158, 204, 0.75) 100%) repeat scroll 0% 0%', description: 'This is an example description' },
 };
 
 export function getColorOfCategory(name: string): string | undefined {
@@ -17,4 +17,8 @@ export function getColorOfCategory(name: string): string | undefined {
 
 export function getCaterogies() {
   return CATEGORIES;
+}
+
+export function getCategoryByName(name: string) {
+  return CATEGORIES[name];
 }
