f0467b03fdca3a903043d71772d737638fba9cb4
k <mol.tired@gmail.com>
2021-05-31 00:51:38 -0500

    make a functional component instead of a class one

--- src/components/ThemePicker/index.tsx
+++ src/components/ThemePicker/index.tsx
@@ -1,50 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import style from './ThemePicker.module.scss';
 
-type Props = {
-  theme:    string
+type ThemePickerProps = {
+  theme: string
   onToggleTheme: (arg0: string) => void
 };
 
-type State = { 
-  active: boolean 
-};
+export default function ThemePicker({ theme, onToggleTheme }: ThemePickerProps) {
+  const [active, setActive] = useState<boolean>(theme === 'dark');
 
-export default class ThemePicker extends Component<Props, State> {
-  state = { active: (this.props.theme === 'dark') };
-
-  onClickHandler = () => {
+  const handleClick = () => {
     // Toggle between dark and light themes
-    const newTheme = this.props.theme === 'dark' ? 'light' : 'dark';
-    this.props.onToggleTheme(newTheme);
-
-    if (newTheme === 'dark') {
-      this.setState(() => ({ active: true }));
-    } else {
-      this.setState(() => ({ active: false }));
-    }
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    onToggleTheme(newTheme);
+    setActive(newTheme === 'dark');
   };
 
-  render() {
-    return (
-      <div className={style.themePicker}>
-        <label className={style.switch}>
-          <input 
-            type="checkbox"
-            className={style.input}
-          />
-          <span 
-            onClick={this.onClickHandler}
-            className={style.span}
-          ></span>
-        </label>
+  return (
+    <div className={style.themePicker}>
+      <label className={style.switch}>
+        <input 
+          type="checkbox"
+          className={style.input}
+        />
         <span 
-          className={`${style.label} 
-            ${this.state.active ? style.labelActive : '' }`}
-        >
-          Dark Mode
-        </span>
-      </div>
-    );
-  }
+          onClick={handleClick}
+          className={style.span}
+        ></span>
+      </label>
+      <span 
+        className={`${style.label} 
+          ${active ? style.labelActive : '' }`}
+      >
+        Dark Mode
+      </span>
+    </div>
+  );
 }
