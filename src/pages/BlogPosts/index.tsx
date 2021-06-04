08c416570204ce1e5ee3e08fe6ab9258bb5ee5dd
k <mol.tired@gmail.com>
2021-06-04 03:52:24 -0500

    use useEffect to fetch the data

--- src/pages/BlogPosts/index.tsx
+++ src/pages/BlogPosts/index.tsx
@@ -1,19 +1,23 @@
+import { useState, useEffect } from 'react';
 import BlogPostShort from 'components/BlogPostShort';
 import { getBlogPosts, IBlogPost } from 'services/BlogPostService';
 
 export default function BlogPosts(): JSX.Element  {
-  const blogPosts: Array<JSX.Element> = getBlogPosts().map((blogPost: IBlogPost) => {
-    return (
-      <BlogPostShort 
-        key={blogPost.title} 
-        title={blogPost.title} 
-        imgUrl={blogPost.imgUrl} 
-        categories={blogPost.categories}
-        subtitle={blogPost.subtitle} 
-        time={blogPost.time} 
-      />
-    );
-  });
+  const [ blogPosts, setBlogPosts ] = useState<Array<JSX.Element>>([]);
+  useEffect(() => {
+    setBlogPosts(getBlogPosts().map((blogPost: IBlogPost) => {
+      return (
+        <BlogPostShort 
+          key={blogPost.title} 
+          title={blogPost.title} 
+          imgUrl={blogPost.imgUrl} 
+          categories={blogPost.categories}
+          subtitle={blogPost.subtitle} 
+          time={blogPost.time} 
+        />
+      );
+    }));
+  }, []);
 
   return <>{blogPosts}</>;
 };
